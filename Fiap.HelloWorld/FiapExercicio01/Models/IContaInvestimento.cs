5fa48fd20a3ff0d05cdcba05213d76dd029038fc
Carolina Goudromihos Puig <85234562+CarolinaPuig@users.noreply.github.com>
2023-08-10 22:59:22 -0300

    Add files via upload
    Realizando exercicio 1

---
+++ Fiap.HelloWorld/FiapExercicio01/Models/IContaInvestimento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapExercicio01.Models
+{
+    internal interface IContaInvestimento
+    {
+        void calculaRetornoInvestimento();
+    }
+}
